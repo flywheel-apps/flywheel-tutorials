96bc1ebb692a1c75b63d2bc01263a4b10d3df15b
qian587 <tanxx587@umn.edu>
2020-09-27 14:58:28 -0400

    modified function on script

--- .script/zd_article_setup.py
+++ .script/zd_article_setup.py
@@ -1,6 +1,6 @@
 import json
 import os
-from .setup import *
+import requests
 
 
 def main():
@@ -41,6 +41,46 @@ def create_article_obj(filepath, title, permission_group_id, user_segment_id):
         "notify_subscribers": False}
 
     return article_dict
+
+
+def publish_article(json_file_path):
+    # Set the target Zendesk subdomain (subdomain.zendesk.com)
+    subdomain = 'flywheelio'
+
+    try:
+        ZD_USER = os.getenv('ZD_USER')
+        ZD_API_TOKEN = os.getenv('ZD_TOKEN')
+        ZD_SECTION_ID = os.getenv('ZD_SECTION_ID')
+    except Exception as e:
+        print(f'Error: {e}')
+
+    # Set the request parameters
+    apiEndPoint = '/api/v2/help_center/sections/' + ZD_SECTION_ID
+    url = 'https://' + subdomain + '.zendesk.com' + apiEndPoint + '/articles'
+
+    # Get the file containing the article in JSON
+    articleJSON = json_file_path
+
+    # New article data packaged in a dictionary matching the expected JSON
+    with open(articleJSON, encoding='utf-8') as data_file:
+        data = json.loads(data_file.read())
+
+    # Encode the data to create a JSON payload
+    payload = json.dumps(data)
+
+    headers = {'content-type': 'application/json'}
+
+    # Do the HTTP post request
+    response = requests.post(url, data=payload, auth=(ZD_USER, ZD_API_TOKEN), headers=headers)
+
+    # Check for HTTP codes other than 201 (Created)
+    if response.status_code != 201:
+        print('Status:', response.status_code, 'Problem with the request. Exiting.')
+        exit()
+    else:
+
+        # Report success
+        print('Successfully created the article.')
 
 
 if __name__ == "__main__":
